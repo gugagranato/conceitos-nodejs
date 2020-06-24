a342c1db49ad492ba8cc36aad8599e7a61d9da31
Gustavo Granato <gustavo.granato@hotmail.com>
2020-06-23 21:41:44 -0300

    challenge 1 - Gostack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,69 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository id!' })
+  }
+  
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+  
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const repository = repositories.find(repo => repo.id === id);
+
+ repository.title = title;
+ repository.url = url;
+ repository.techs = techs;
+
+  return response.status(200).json(repository);
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repo => repo.id === id);
+
+  repository.likes++;
+
+  return response.status(200).json(repository)
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
+const express = require('express');
 const app = require("./app");
+
+const server = express();
+
+server.use(app);
 
 app.listen(3333);
